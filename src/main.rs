7159518e95f18d695da2997090b7f5ed18c63f71
cumulus13 <cumulus13@gmail.com>
2026-05-31 18:43:37 +0700

    version: 0.1.1 ~ 2026-05-31 18:43:30:672105

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,15 @@ mod theme;
 
 use anyhow::Result;
 use clap::Parser;
+use clap_version_flag::colorful_version;
 use cli::Cli;
 
 fn main() -> Result<()> {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() == 2 && (args[1] == "-V" || args[1] == "--version") {
+        let version = colorful_version!();
+        version.print_and_exit();
+    }
     let cli = Cli::parse();
     cli.run()
 }
